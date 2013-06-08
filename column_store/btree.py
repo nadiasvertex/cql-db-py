65e0568a96d06ff993495b6b10c3cf742113e83f
Christopher Nelson <nadiasvertex@gmail.com>
2013-06-08 14:37:52 -0400

    Work on the btree implementation.

--- column_store/btree.py
+++ column_store/btree.py
@@ -12,16 +12,22 @@ import struct
 
 from column_store import page
 from column_store import buffer
+from column_store import file_manager
+
+HEADTREE_PAGES_BOUND = 256
 
 class TreeState(object):
-   __slots__ = ["state_type", "fdtree", "file_id", "file_handle", "buffer", "mbuffer",
-                "current_page", "current_page_head", "current_entries", "current_offset",
+   __slots__ = ["state_type", "tree", "file_id", "file_handle", "buffer", "mbuffer",
+                "current_page", "current_entries", "current_offset",
                 "page_id", "level", "force_write", "force_read", "tree", "next_state"]
-   def __init__(self):
-      self.state_type = 0
-      self.fdtree = None
-      self.file_id = 0
-      self.file_handle = None
+
+   STATE_TYPE_BTREE = 1
+
+   def __init__(self, tree, file_id, file_handle):
+      self.state_type = self.STATE_TYPE_BTREE
+      self.tree = tree
+      self.file_id = file_id
+      self.file_handle = file_handle
       self.buffer = None
       self.mbuffer = None
       self.current_page = None
@@ -38,41 +44,58 @@ class TreeState(object):
 class BTree(object):
    __slots__ = ["file_id", "file_handle", "level_block", "max_block", "max_entry",
                 "entry_count", "block_count", "level_count", "root", "init_entry", "buffer",
-                "entry_factory", "page_buffer"]
+                "entry_factory", "page_buffer", "file_manager"]
    def __init__(self, entry_factory=page.EntryFactory):
-      self.file_id = []
-      self.file_handle = []
-      self.level_block = []
-      self.max_block = 0
-      self.max_entry = 0
-      self.block_count = 0
-      self.entry_count = 0
-      self.level_count = 0
-      self.root = 0
-      self.init_entry = 0
-      self.buffer = None
+      self.file_manager = file_manager.Manager()
       self.entry_factory = entry_factory()
       self.page_buffer = buffer.Manager()
+      file_id, file_handle = self.file_manager.allocate_file()
+      self.file_id = [file_id]
+      self.file_handle = [file_handle]
+      self.level_block = [0]
+      self.max_block = HEADTREE_PAGES_BOUND
+      self.max_entry = self.max_block * self.entry_factory.get_entry_count_max()
+      self.block_count = 1
+      self.entry_count = 1
+      self.level_count = 1
+      self.root = 0
+      self.init_entry = 1
+      self.buffer = None
+
+      page_data = self.page_buffer.new_page(file_handle, file_id, self.level_block[0])
+      p = page.Page(page_data, self.entry_factory)
+
+      # Insert the sentinel element
+      e = page.Entry(page.MAX_KEY_VALUE, page.INVALID_PAGE)
+      p.insert(e)
+      e.ptr |= (page.Page.FENCE | page.Page.INTERNAL)
+
+      self.page_buffer.mark_dirty(file_id, self.level_block[0])
+      self.level_block[0] += 1
+
+   def _read_page(self, level, page_id):
+      data = self.page_buffer.read_page(self.file_handle[level],
+                                        self.file_id[level], page_id)
+      return page.Page(data, self.entry_factory)
 
    def insert(self, entry):
-      # int offset1;
-      # Entry * data1;
       key = entry.key
       page_id = self.root
       trace = []
 
-      for i in range(0, self.level_count):
+      for i in range(0, self.level_count - 1):
          current_level = self.level_count - i - 1
          trace[current_level] = page_id
-         cur_page = self.read_page(self.file_handle[current_level], self.file_id[current_level], page_id)
-         cur_entry = self.in_page_search(page, key)
-         page_id = cur_entry.ptr
+         cur_page = self._read_page(current_level, page_id)
+         index = cur_page.search(key)
+         entry = cur_page.entries[index]
+         page_id = entry.ptr & page.Page.MASK
 
-      final_page = self.read_page(self.file_handle[0], self.file_id[0], page_id)
-      self.in_page_insert(final_page, entry)
-      self.mark_dirty(self.file_id[0], page_id)
+      final_page = self._read_page(0, page_id)
+      final_page.insert(entry)
+      self.page_buffer.mark_dirty(self.file_id[0], page_id)
 
-      if final_page.entry_count >= self.entry_factory.get_entry_count_max():
+      if final_page.header.num >= self.entry_factory.get_entry_count_max():
          self.split(final_page, 0, trace)
 
       self.entry_count += 1
