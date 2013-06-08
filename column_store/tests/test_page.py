57feac9a526d35ca80a74e585782308ec769fef1
Christopher Nelson <nadiasvertex@gmail.com>
2013-06-08 11:24:24 -0400

    Added more tests to ensure that insertion works regardless of order.

--- column_store/tests/test_page.py
+++ column_store/tests/test_page.py
@@ -6,6 +6,7 @@ Created on Jun 8, 2013
 
 import io
 import os
+import random
 import unittest
 
 class TestPage(unittest.TestCase):
@@ -23,7 +24,7 @@ class TestPage(unittest.TestCase):
 
       self.assertEqual(1, p.header.num)
 
-   def test_can_insert_many(self):
+   def test_can_insert_many_backwards(self):
       from column_store import page
       p = page.Page(bytearray(self.BLOCK_SIZE), page.EntryFactory())
 
@@ -34,3 +35,44 @@ class TestPage(unittest.TestCase):
       self.assertEqual(100, p.header.num)
       for i in range(1, 101):
          self.assertEqual(i, p.entries[i - 1].key)
+
+   def test_can_insert_many_forwards(self):
+      from column_store import page
+      p = page.Page(bytearray(self.BLOCK_SIZE), page.EntryFactory())
+
+      for i in range(1, 101):
+         e = page.Entry(i, 100 + i)
+         p.insert(e)
+
+      self.assertEqual(100, p.header.num)
+      for i in range(1, 101):
+         self.assertEqual(i, p.entries[i - 1].key)
+
+
+   def test_can_insert_many_random(self):
+      from column_store import page
+      p = page.Page(bytearray(self.BLOCK_SIZE), page.EntryFactory())
+
+      keys = [i for i in range(1, 101)]
+      random.shuffle(keys)
+
+      for i in range(0, 100):
+         e = page.Entry(keys[i], 101 + i)
+         p.insert(e)
+
+      self.assertEqual(100, p.header.num)
+      for i in range(1, 101):
+         self.assertEqual(i, p.entries[i - 1].key)
+
+
+   def test_can_search(self):
+      from column_store import page
+      p = page.Page(bytearray(self.BLOCK_SIZE), page.EntryFactory())
+
+      for i in range(100, 0, -1):
+         e = page.Entry(i, 100 + i)
+         p.insert(e)
+
+      index = p.search(50)
+      self.assertEqual(50, index)
+
