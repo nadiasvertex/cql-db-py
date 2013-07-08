dd2f525c9fc4014222436151a1344904ff8c47dd
Christopher Nelson <nadiasvertex@gmail.com>
2013-07-08 08:58:14 -0400

    Created datafile namespace, fixed tests that were broken by the move.

--- native/src/cpp/test/test_index.cpp
+++ native/src/cpp/test/test_index.cpp
@@ -13,35 +13,35 @@
 
 using namespace std;
 
-const int test_reps = 10;
+const int test_reps = 10000;
 
 // Tests that we can create a cql::index object.
 TEST(IndexFileTest, CanCreate) {
-	auto test = [] {std::unique_ptr<cql::index>(new cql::index("test.idx"));};
+	auto test = [] {std::unique_ptr<cql::datafile::index>(new cql::datafile::index("test.idx"));};
 	EXPECT_NO_THROW(test());
 }
 
 TEST(IndexFileTest, CanWrite) {
-	cql::index idx("test.idx");
+	cql::datafile::index idx("test.idx");
 
 	ASSERT_TRUE(idx.is_open());
 
 	for (int i = 0; i < test_reps; i++) {
-		auto pos = cql::entry_position::from_uint64(i);
+		auto pos = cql::datafile::entry_position::from_uint64(i);
 		idx.put_entry_offset(pos, i * 100);
 	}
 }
 
 TEST(IndexFileTest, CanRead) {
-	cql::index idx("test.idx");
+	cql::datafile::index idx("test.idx");
 
 	for (int i = 0; i < test_reps; i++) {
-		auto pos = cql::entry_position::from_uint64(i);
+		auto pos = cql::datafile::entry_position::from_uint64(i);
 		idx.put_entry_offset(pos, i * 100);
 	}
 
 	for (int i = 0; i < test_reps; i++) {
-		auto pos = cql::entry_position::from_uint64(i);
+		auto pos = cql::datafile::entry_position::from_uint64(i);
 		auto offset = idx.get_entry_offset(pos);
 
 		ASSERT_EQ(i*100, offset);
