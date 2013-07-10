c6fbbe8439a1d98b7d7ee61ef1f8dc73508cd48b
Christopher Nelson <nadiasvertex@gmail.com>
2013-07-09 20:07:38 -0400

    Working on data store file.

--- native/src/cpp/test/test_store.cpp
+++ native/src/cpp/test/test_store.cpp
@@ -17,7 +17,8 @@ const int test_reps = 10000;
 
 // Tests that we can create a cql::store<int> object.
 TEST(StoreTest, CanCreate) {
-	auto test = [] {std::unique_ptr<cql::datafile::store<int>>(new cql::datafile::store<int>("test"));};
+	auto test =
+			[] {std::unique_ptr<cql::datafile::store<int>>(new cql::datafile::store<int>("test"));};
 	EXPECT_NO_THROW(test());
 }
 
@@ -27,22 +28,36 @@ TEST(StoreTest, CanWrite) {
 	ASSERT_TRUE(st.is_open());
 
 	for (int i = 0; i < test_reps; i++) {
-		st.put(i, i*1000);
+		st.put(i, i * 1000);
 	}
 }
 
-/*TEST(StoreTest, CanRead) {
+TEST(StoreTest, CanWriteDuplicates) {
 	cql::datafile::store<int> st("test");
 
-	for (int i = 0; i < test_reps; i++) {
-		auto pos = cql::datafile::entry_position::from_uint64(i);
-		st.put_entry_offset(pos, i * 100);
+	ASSERT_TRUE(st.is_open());
+
+	for (int j = 0; j < test_reps/10; j++) {
+		for (int i = 0; i < test_reps/10; i++) {
+			st.put(i, j);
+		}
 	}
 
-	for (int i = 0; i < test_reps; i++) {
-		auto pos = cql::datafile::entry_position::from_uint64(i);
-		auto offset = st.get_entry_offset(pos);
+	ASSERT_EQ(test_reps/10, st.count());
+}
 
-		ASSERT_EQ(i*100, offset);
-	}
-}*/
+/*TEST(StoreTest, CanRead) {
+ cql::datafile::store<int> st("test");
+
+ for (int i = 0; i < test_reps; i++) {
+ auto pos = cql::datafile::entry_position::from_uint64(i);
+ st.put_entry_offset(pos, i * 100);
+ }
+
+ for (int i = 0; i < test_reps; i++) {
+ auto pos = cql::datafile::entry_position::from_uint64(i);
+ auto offset = st.get_entry_offset(pos);
+
+ ASSERT_EQ(i*100, offset);
+ }
+ }*/
