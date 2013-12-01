71edb467b81675c68c24186ca14fdf8d512bba80
Christopher Nelson <nadiasvertex@gmail.com>
2013-12-01 08:56:13 -0500

    Cache flushing works.

--- cql/cache.py
+++ cql/cache.py
@@ -6,7 +6,7 @@ class Cache(object):
    Over time, in the background, data is migrated into the persistent store.
    """
    def __init__(self, database, username, password):
-      self.persistent_store = database.connect()
+      self.store = database.connect()
       self.cache = sqlite3.connect(":memory:")
       self._create_schema()
 
@@ -14,7 +14,7 @@ class Cache(object):
       self.flush()
 
    def _get_persistent_store_tables(self):
-      cur = self.persistent_store.cursor()
+      cur = self.store.cursor()
       cur.execute("SELECT name, type FROM sys.tables WHERE system=false;")
       tables = cur.fetchall()
       cur.close()
@@ -24,7 +24,7 @@ class Cache(object):
       from model import ModelMeta
       from common import ENGINE_SQLITE3, ENGINE_MONETDB
 
-      cur = self.persistent_store.cursor()
+      cur = self.store.cursor()
       persistent_tables = set(self._get_persistent_store_tables())
       for n, m in ModelMeta.models.iteritems():
          # Always create the table in the cache, since the cache disappears when closed.
@@ -42,21 +42,47 @@ class Cache(object):
    def flush(self):
       self.move()
 
-   def move(self, count=-1):
+   def move(self, count=0):
       from model import ModelMeta
       from common import ENGINE_SQLITE3, ENGINE_MONETDB
 
-      cur = self.persistent_store.cursor()
+      scur = self.store.cursor()
+      ccur = self.cache.cursor()
       for n, m in ModelMeta.models.iteritems():
          for row in self.cache.execute("SELECT COUNT(*) FROM " + m.get_table_name()):
             row_count = row[0]
             if row_count==0:
                continue
 
-            move_count=min(count, row_count)
-            field_names = ",".join(m.get_fields().names())
-            from_sql = "SELECT " + field_names + " FROM " + m.get_table_name() + " LIMIT " + str(move_count)
+            # Figure out how many rows to move
+            move_count = row_count if count < 1 else min(count, row_count)
 
+            # Prepare to move the data
+            field_names_list = m.get_fields().keys()
+            field_names = ",".join(field_names_list)
+            values = ",".join(["%s" for x in range(0, len(field_names_list))])
+            from_sql = "SELECT " + field_names + " FROM " + m.get_table_name() + " LIMIT " + str(move_count)
+            to_sql = "INSERT INTO " + m.get_table_name() + "(" + field_names +\
+                     ") VALUES (" + values + ")"
+
+            # Load rows up from the cache
+            ccur.execute(from_sql)
+            data = ccur.fetchall()
+
+            # Write them in the persistent store
+            scur.executemany(to_sql, data)
+
+            # Delete the data from the cache
+            pk_name = m.get_primary_key_name()
+            pk_index = field_names_list.index(pk_name)
+            id_values = [str(row[pk_index]) for row in data]
+            ccur.execute("DELETE FROM " + m.get_table_name() + " WHERE " +
+                        pk_name + " IN (" + ",".join(id_values) + ")")
+
+      # Commit the transaction
+      ccur.close()
+      scur.close()
+      self.store.commit()
 
    def execute(self, sql):
       return self.cache.execute(sql)
@@ -123,6 +149,32 @@ if __name__ == "__main__":
             rowcount+=1
 
          self.assertEqual(0, rowcount)
+
+      def testFlushDataToStore(self):
+         am = Address.new(self.cache)
+         am.addr_type = 10
+         am.save()
+
+         self.cache.flush()
+
+         # Now check to see if we got the data into the store.
+         cur = self.cache.store.cursor()
+         cur.execute("SELECT address_id, addr_type from address;")
+
+         rowcount=0
+         for row in cur.fetchall():
+            rowcount+=1
+            self.assertEqual(10, row[1])
+
+         self.assertEqual(1, rowcount)
+
+         # Finally, make sure the data is out of the cache
+         rowcount=0
+         for row in self.cache.execute("SELECT address_id, addr_type from address;"):
+            rowcount+=1
+         self.assertEqual(0, rowcount)
+
+
 
 
    unittest.main()
